6ce381551fb09b2a1c1d927331a37d58acb45ad4
swadhinrajbehera@gmail.com <swadhinrajbehera@gmail.com>
2026-03-08 18:11:03 +0530

    inittial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,3 @@
+<h1>our first page </h1>
+
+<a href = "/post">Post page</a>
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+// Route::get('/post', function () {
+//     return view('post');
+// });
+
+
+//second method
+//Route::view('/post', 'post');
+
+
+
+//parameterized route
+Route::get('/post/{id?}', function ($id = null) {
+    if($id){
+        return "<h1>Post id is:".$id."</h1>";
+    }else{
+    return "<h1>No post id provided</h1>";
+    }
+});
