a980c11aa911c91920295dfc30597090daa80774
swadhinrajbehera@gmail.com <swadhinrajbehera@gmail.com>
2026-03-09 02:05:57 +0530

    inittial commit

---
+++ resources/views/pages/header.blade.php
@@ -0,0 +1,3 @@
+<h1>header page </h1>
+
+<p> {{$name}} </p>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,12 @@
-<h1> Home : first page</h1>
+@include('pages.header',['name' => 'swadhin'])
 
-{{-- <a href = "/post">Post page</a> --}}
+
+<h1> Home : first page</h1>
+@include('pages.footer')
+
+
+
+{{-- <a href = "/post">Post page</a>
 
 {{5+2}}
 
@@ -11,29 +17,29 @@
 <br><br>
 
 {{"<h1>hi swadhinraj</h1>"}}
-{!!"<h1>hi swadhinraj</h1>"!!}
+{!!"<h1>hi swadhinraj</h1>"!!} --}}
 
 
 {{-- {!!"<script>alert('hi swadhinraj');</script>"!!} --}}
 
-@php
-    // $name = "swadhinraj";
+{{-- @php --}}
+    {{-- // $name = "swadhinraj";
     $name = ["swadhinraj", "raj", "dino"];
-@endphp
+@endphp --}}
 {{-- {{$name}} --}}
-<ul>
-@foreach ( $name as $n)
+{{-- <ul>
+@foreach ( $name as $n) --}}
     {{-- <li>{{$n}}</li> --}}
         {{-- <li>{{ $loop->index}} -{{$n}}</li> --}}
                 {{-- <li>{{ $loop->iteration }} -{{$n}}</li> --}}
         {{-- <li>{{ $loop->count}} -{{$n}}</li> --}}
        
-@endforeach
+{{-- @endforeach
 </ul>
 
 
 
-{{$name = "swadhinraj2"}}
+{{$name = "swadhinraj2"}} --}}
 
 
 
