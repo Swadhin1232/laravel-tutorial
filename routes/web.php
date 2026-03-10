fbbd734ed520c11b35acc60a50b4f74f88fab966
swadhinrajbehera@gmail.com <swadhinrajbehera@gmail.com>
2026-03-11 01:02:42 +0530

    inittial commit

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PageController;
+// use App\Http\Controllers\PageController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', [PageController::class, 'showHome']);
-Route::get('/user', [PageController::class, 'showUser']);
+// Route::get('/', [PageController::class, 'showHome']);
+// Route::get('/user', [PageController::class, 'showUser']);
 
 
 
